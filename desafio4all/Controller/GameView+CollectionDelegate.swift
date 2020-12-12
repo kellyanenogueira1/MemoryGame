54c88b0c5291c22c2fef8c311db5bca7d017821c
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-12 19:27:20 -0300

    gameViewDelegate

---
+++ desafio4all/Controller/GameView+CollectionDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  GameView+CollectionDelegate.swift
+//  desafio4all
+//
+//  Created by Lidiane Gomes Barbosa on 12/12/20.
+//
+
+import Foundation
---
+++ desafio4all/View/GameCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameCollectionViewCell.swift
+//  desafio4all
+//
+//  Created by Lidiane Gomes Barbosa on 12/12/20.
+//
+
+import UIKit
+
+class GameCollectionViewCell: UICollectionViewCell {
+    
+}
