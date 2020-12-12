dc7ff80fc34af483afaa316122a5a8becd4ac86b
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-12 15:30:25 -0300

    interface test

--- desafio4all/Controller/ViewController.swift
+++ desafio4all/Controller/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  desafio4all
 //
 //  Created by Lidiane Gomes Barbosa on 11/12/20.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
