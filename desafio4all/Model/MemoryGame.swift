b0e3c9d9f8b9e0805323f975f4dd70fc0b0b87f4
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-14 15:27:42 -0300

    model memoryGame

--- desafio4all/Model/MemoryGame.swift
+++ desafio4all/Model/MemoryGame.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+class MemoryGame {
+    var cards = [Card]()
+    var cardsShown = [Card]()
+    var isPlaying: Bool = false
+    weak var delegate: MemoryGameDelegate?
+    
+    func newGame(totalCards: Int) -> [Card] {
+        self.cards = generateshuffleCards(totalCards: totalCards)
+        self.isPlaying = true
+        return cards
+    }
+    
+    func restartGame() {
+        self.isPlaying = false
+        self.cards.removeAll()
+        self.cardsShown.removeAll()
+    }
+    
+    func isPossibleComparation() -> Bool {
+        return cardsShown.count % 2 != 0
+    }
+    
+    func lastCardPressed() -> Card? {
+        return cardsShown.last
+    }
+    
+    func didSelectCard(card: Card?) {
+        guard let card = card else { return }
+        card.isShown = true
+        
+        if isPossibleComparation() {
+            let lastCard = lastCardPressed()!
+            
+            if card.isEqual(lastCard) {
+                cardsShown.append(card)
+                card.delegate?.success()
+                lastCard.delegate?.success()
+            } else {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    card.isShown = false
+                    lastCard.isShown = false
+                }
+                cardsShown.removeLast()
+            }
+        } else {
+            cardsShown.append(card)
+        }
+        
+        if cardsShown.count == cards.count {
+            endGame()
+        }
+        
+    }
+    
+    func endGame() {
+        delegate?.didEndGame()
+    }
+    
+    private func generateshuffleCards(totalCards: Int) -> [Card] {
+        var newCards = [Card]()
+        let sequence = generateSequence(totalCards: totalCards)
+        sequence.forEach { value in
+            let card = Card(id: value)
+            newCards.append(card)
+        }
+        return newCards
+    }
+    
+    
+    private func generateSequence(totalCards: Int) -> [Int] {
+        var sequence: [Int] = []
+        let totalNumbers = totalCards / 2
+        sequence.append(contentsOf: 1...totalNumbers)
+        sequence.append(contentsOf: 1...totalNumbers)
+        
+        return sequence.shuffled()
+    }
+}
