5c8a632d293d6583ef8caee012c2f52361086718
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-15 13:25:11 -0300

    Colors and text properties in main Vc

--- desafio4all/Controller/MainViewController.swift
+++ desafio4all/Controller/MainViewController.swift
@@ -14,8 +14,17 @@ class MainViewController: UIViewController {
     @IBOutlet weak var buttonHardLevel: UIButton!
     @IBOutlet weak var buttonHistoric: UIButton!
     
+    let titleView: UILabel = {
+        let title = UILabel()
+        title.text = "Jogo da Memória"
+        title.textColor = .textColor
+        title.font = UIFont.systemFont(ofSize: 45)
+        return title
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
     }
 
     @IBAction func goEasyLevel(_ sender: Any) {
@@ -30,6 +39,39 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func goHistoric(_ sender: Any) {
+    }
+    
+    func configureUI() {
+        view.tintColor = .textColor
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.barTintColor = .magentaBackGround
+        navigationController?.navigationBar.isTranslucent = false
+        navigationItem.titleView = titleView
+       
+        view.backgroundColor = .magentaBackGround
+        
+        buttonEasyLevel.titleLabel?.text = "4X3"
+        layout(view: buttonEasyLevel)
+        
+        buttonMediumLevel.titleLabel?.text = "4X4"
+        layout(view: buttonMediumLevel)
+        
+        buttonHardLevel.titleLabel?.text = "5X4"
+        layout(view: buttonHardLevel)
+        
+        buttonHistoric.titleLabel?.text = "Histórico"
+        layout(view: buttonHistoric)
+        
+    }
+    
+    func layout(view: UIView) {
+        view.backgroundColor = .greenAction
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 3)
+        view.layer.shadowRadius = 5
+        view.layer.shadowOpacity = 0.3
     }
     
     func showGame(rows: Int, columns: Int) {
