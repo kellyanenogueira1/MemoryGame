0b5bc8bf2d44b4141706cdc01a5dfdd2ade27cba
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-15 13:24:23 -0300

    fix name color

--- desafio4all/Utils/ColorExtension.swift
+++ desafio4all/Utils/ColorExtension.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 extension UIColor {
     static let magentaBackGround = UIColor(red: 143/255, green: 45/255, blue: 86/255, alpha: 1)
-    static let textColors = UIColor(red: 214/255, green: 243/255, blue: 240/255, alpha: 1)
+    static let textColor = UIColor(red: 214/255, green: 243/255, blue: 240/255, alpha: 1)
     static let greenAction = UIColor(red: 65/255, green: 157/255, blue: 120/255, alpha: 1)
     static let yellowAction = UIColor(red: 255/255, green: 188/255, blue: 66/255, alpha: 1)
 }
