19dea9f8814699aaa8f8ac7ec0596d0c35fe9f34
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-17 11:25:17 -0300

    add coredata

--- desafio4all/Controller/MainViewController.swift
+++ desafio4all/Controller/MainViewController.swift
@@ -6,7 +6,10 @@
 //
 
 import UIKit
-
+import CoreData
+enum Level: Int {
+    case easy = 1, medium = 2, hard = 3
+}
 class MainViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
@@ -14,21 +17,28 @@ class MainViewController: UIViewController {
     @IBOutlet weak var buttonMediumLevel: UIButton!
     @IBOutlet weak var buttonHardLevel: UIButton!
     @IBOutlet weak var buttonHistoric: UIButton!
+    var context: NSManagedObjectContext!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        setContext()
+    }
+    
+    func setContext() {
+        context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.automaticallyMergesChangesFromParent = true
     }
     
     @IBAction func goEasyLevel(_ sender: Any) {
-        showGame(rows: 4, columns: 3)
+        showGame(rows: 4, columns: 3, level: .easy)
     }
     
     @IBAction func goMediumLevel(_ sender: Any) {
-        showGame(rows: 4, columns: 4)
+        showGame(rows: 4, columns: 4, level: .medium)
     }
     @IBAction func goHardLevel(_ sender: Any) {
-        showGame(rows: 5, columns: 4)
+        showGame(rows: 5, columns: 4, level: .hard)
     }
     
     @IBAction func goHistoric(_ sender: Any) {
@@ -78,11 +88,13 @@ class MainViewController: UIViewController {
         view.layer.shadowOpacity = 0.1
     }
     
-    private func showGame(rows: Int, columns: Int) {
+    private func showGame(rows: Int, columns: Int, level: Level) {
         let storyboard = UIStoryboard(name: "Game", bundle: nil)
         let viewC = storyboard.instantiateViewController(identifier: "Game", creator: { coder in
             return GameViewController(coder: coder, rows: rows, collumns: columns)
-        }) 
+        })
+        viewC.context = context
+        viewC.level = level
         navigationController?.pushViewController(viewC, animated: true)
     }
     
@@ -90,6 +102,7 @@ class MainViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Historic", bundle: nil)
         guard let viewC = storyboard.instantiateViewController(identifier: "historic") as? HistoricViewController else { return }
         viewC.modalPresentationStyle = .fullScreen
+        viewC.context = context
         self.present(viewC, animated: true, completion: nil)
     }
 }
