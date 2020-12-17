4abfcba7a179bab45f1eecb79c28e420060b96a7
Lidiane <lidianegomesbarbosa@gmail.com>
2020-12-17 11:29:54 -0300

    modified xctdatamodel

--- desafio4all/AppDelegate.swift
+++ desafio4all/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentCloudKitContainer(name: "MemoryGame")
+        let container = NSPersistentContainer(name: "Game")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
 
